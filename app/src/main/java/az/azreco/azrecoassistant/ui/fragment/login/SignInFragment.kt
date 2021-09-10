ce423402398d5af0df2e7110da036ad76beafa0f
fu7ur3gho57 <fu7ur3gh057@gmail.com>
2021-09-10 10:35:59 +0400

    Sign In and Sign Up UI

--- app/src/main/java/az/azreco/azrecoassistant/ui/fragment/login/SignInFragment.kt
+++ app/src/main/java/az/azreco/azrecoassistant/ui/fragment/login/SignInFragment.kt
@@ -1,18 +1,17 @@
 package az.azreco.azrecoassistant.ui.fragment.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import az.azreco.azrecoassistant.R
-import az.azreco.azrecoassistant.databinding.FragmentChatBinding
 import az.azreco.azrecoassistant.databinding.FragmentSignInBinding
-import az.azreco.azrecoassistant.ui.viewmodel.DialogViewModel
+import az.azreco.azrecoassistant.ui.activity.MainActivity
 import az.azreco.azrecoassistant.ui.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SignInFragment : Fragment(R.layout.fragment_sign_in) {
@@ -26,39 +25,41 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSignInBinding.bind(view)
-
-        loginClickListener()
+        signInClickListener()
+        redirectToSignUp()
     }
 
-    private fun loginClickListener() = binding.loginBtn.setOnClickListener {
+    private fun signInClickListener() = binding.signInBtn.setOnClickListener {
         if (filterLoginData()) {
             // viewModel signIn()
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            requireContext().startActivity(intent)
         }
     }
 
     private fun filterLoginData(): Boolean {
         binding.apply {
-            val emailText = loginEmailEt.text
-            val passText = loginPasswordEt.text
-            if (emailText.length < 5 || !emailText.contains("@")) {
+            val emailText = signInEmailEt.text
+            val passText = signInPasswordEt.text
+            return if (emailText.length < 5 || !emailText.contains("@")) {
                 showText("Email is Empty")
-                return false
+                false
             } else if (passText.length < 3) {
                 showText("Password is empty")
-                return false
+                false
             } else {
                 showText("DONE!")
-                return true
+                true
             }
         }
     }
 
-    private fun forgorPasswordClickListener() = binding.loginForgotPassTv.setOnClickListener {
+    private fun forgorPasswordClickListener() = binding.signInForgotPassTv.setOnClickListener {
 
     }
 
-    private fun signUpClickListener() = binding.loginSignUpTv.setOnClickListener {
-
+    private fun redirectToSignUp() = binding.signInRedirectTv.setOnClickListener {
+        findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
     }
 
     private fun showText(text: String) = Toast.makeText(
--- app/src/main/java/az/azreco/azrecoassistant/ui/fragment/login/SignUpFragment.kt
+++ app/src/main/java/az/azreco/azrecoassistant/ui/fragment/login/SignUpFragment.kt
@@ -1,21 +1,34 @@
 package az.azreco.azrecoassistant.ui.fragment.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import az.azreco.azrecoassistant.R
-import az.azreco.azrecoassistant.databinding.FragmentSignInBinding
+import az.azreco.azrecoassistant.databinding.FragmentSignUpBinding
+import az.azreco.azrecoassistant.ui.activity.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+    private val TAG = "SignUpFragment"
 
-    private lateinit var binding: FragmentSignInBinding
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentSignInBinding.bind(view)
+        binding = FragmentSignUpBinding.bind(view)
+        signUpClickListener()
+        redirectToSignIn()
     }
 
+    private fun signUpClickListener() = binding.signUpBtn.setOnClickListener {
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        requireContext().startActivity(intent)
+    }
 
+    private fun redirectToSignIn() = binding.signUpRedirectTv.setOnClickListener {
+        findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
+    }
 }
